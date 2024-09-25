4eceea29136f4cbd95bcab009eed72a9a269262c
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-09-24 23:03:45 -0700

    finished the project homework for The Odin Project

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const fs = require("fs");
 const http = require("http");
 
 const homepage = fs.readFileSync("index.html", "utf-8");
+const about = fs.readFileSync("about.html", "utf-8");
+const contact = fs.readFileSync("contact.html", "utf-8");
 
 const server = http
   .createServer((req, res) => {
     if (req.url === "/") {
-      res.writeHead(200, { "Content-Type": "txt/html" });
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.write(homepage);
+      res.end();
+    } else if (req.url === "/about") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(about);
+      res.end();
+    } else if (req.url === "/contact") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(contact);
+      res.end();
     } else {
       res.end("error 404, not found");
     }
